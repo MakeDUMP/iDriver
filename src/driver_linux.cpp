212b4c9f63223b9b4d8fb8de484928ff1931a2c9
Alexander U.Jurankov <heir@mail.ru>
2023-11-08 21:40:17 +0300

    Refactoring

--- src/driver_linux.cpp
+++ src/driver_linux.cpp
@@ -6,10 +6,9 @@
 
 modeinfo_section static const char name[] = "name=iDriver";
 modeinfo_section static const char author[] = "author=Alexander U.Jurankov <heir@mail.ru> MakeDUMP";
-modeinfo_section static const char description[] = "description=Universal PCIe driver";
+modeinfo_section static const char description[] = "description=Universal PCIe Driver";
 modeinfo_section static const char license[] = "license=GPL v2";
 modeinfo_section static const char retpoline[] = "retpoline=Y";
-modeinfo_section static const char vermagic[] = "vermagic=6.3.0-generic SMP preempt mod_unload modversions ";
 versions_section static const char versions[] = "\x00\x00\x00\x00\x00\x00\x00\x00";
 
 extern "C" int init_module() { return driver_init(); }
@@ -28,9 +27,11 @@ extern "C" void cleanup_module(void) { driver_clean(); }
 // 0000000000000490 R_X86_64_64              cleanup_module
 
 #if defined(linux_630)
+modeinfo_section static const char vermagic[] = "vermagic=6.3.0-generic SMP preempt mod_unload modversions ";
 constexpr auto this_module_section_size = 896;
 constexpr auto cleanup_offset = 824; // 0x338
 #else
+modeinfo_section static const char vermagic[] = "vermagic=6.5.0-generic SMP preempt mod_unload modversions ";
 constexpr auto this_module_section_size = 1280;
 constexpr auto cleanup_offset = 1168; // 0x490
 #endif
@@ -41,23 +42,11 @@ module_section extern "C" struct {
     estd::uint8_t reserved_2[232];
     int (*init)(void); // Startup function
 
-    estd::uint8_t skip_3[504];
-    // estd::uint8_t reserved_3[(cleanup_offset - sizeof(reserved_1) - sizeof(name) - sizeof(reserved_2) - sizeof(init))];
+    estd::uint8_t reserved_3[(cleanup_offset - sizeof(reserved_1) - sizeof(name) - sizeof(reserved_2) - sizeof(init))];
     void (*exit)(void); // Destruction function
-    estd::uint8_t skip_4[64];
-    // estd::uint8_t reserved_4[this_module_section_size - cleanup_offset - sizeof(exit)];
-} __attribute__((__packed__)) __this_module = { .name = "iDriver", .init = init_module, .exit = cleanup_module };
-
-// module_section extern "C" struct { // total: 896 bytes
-//     estd::uint8_t skip_1[24];
-//     char name[64 - sizeof(unsigned long)]; // 56 для 64-x бит
-//     estd::uint8_t skip_2[232];
-//     int (*init)(void); // Startup function
-//     estd::uint8_t skip_3[504];
-//     void (*exit)(void); // Destruction function
-//     estd::uint8_t skip_4[64];
-// } __this_module = {
-//     .name = "iDriver",
-//     .init = init_module,
-//     .exit = cleanup_module,
-// };
+    estd::uint8_t reserved_4[this_module_section_size - cleanup_offset - sizeof(exit)];
+} __attribute__((__packed__)) __this_module = {
+    .name = "iDriver",
+    .init = init_module,
+    .exit = cleanup_module
+};
